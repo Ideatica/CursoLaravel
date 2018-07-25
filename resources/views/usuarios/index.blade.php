591bf94fbddd630f6aae7dc18b9042fd0eb1dc22
crissrojas <41449765+crissrojas@users.noreply.github.com>
2018-07-25 14:53:27 -0400

    icono para aprobar

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -28,6 +28,12 @@
                         <a href="{{ route('usuarios.delete', $usuario->id) }}" class="btn btn-xs btn-danger" data-toggle="tooltip" title="Eliminar Usuario">
                             <i class="fa fa-trash"></i>
                         </a>
+
+                        @if (!$usuario->hasRole('user'))
+
+                        <a href="{{ route('usuarios.approve', $usuario->id) }}" class="btn btn-xs btn-warning" data-toggle="tooltip" title="Aprobar Usuario">
+                            <i class="fa fa-check"></i>
+                        </a> @endif
                     </td>
                 </tr>
                 @endforeach
